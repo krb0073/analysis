3d6e79d6e95c99529843a0fb5a5cba7eea26e50f
krb0073 <krb0073@mix.wvu.edu>
2021-10-12 21:54:55 -0400

    changed to common format using autopep8

--- regression/Main_regression_code.py
+++ regression/Main_regression_code.py
@@ -4,6 +4,7 @@
 # @Filename: Main_regression_code.py
 # @Last modified by:   kbillings
 # @Last modified time: 2021-03-04T23:47:12-05:00
+from V2_regression import *
 import os
 import sys
 import glob
@@ -21,15 +22,22 @@ import loos.pyloos
 import pandas as pd
 import numpy as np
 import scipy
-from math import log ,ceil
+from math import log, ceil
 sys.path.append('/media/bak12/Analysis/regression/')
-from V2_regression import *
 config_file = sys.argv[1]
+
+
 def main(config_file):
     config = readConfig(config_file)
     # define the common stuff for all the metods
-    reg = OLS_regression(config.data_range[0],config.data_range[1],config.Lmax,config.dirloc,config.seedNum,config.num_var)
-    if reg.keep != config.keep_num :
+    reg = OLS_regression(
+        config.data_range[0],
+        config.data_range[1],
+        config.Lmax,
+        config.dirloc,
+        config.seedNum,
+        config.num_var)
+    if reg.keep != config.keep_num:
         reg.keep = config.keep_num
     # gen the random seed
     reg.RandomSeed()
@@ -39,22 +47,32 @@ def main(config_file):
             reg.skip = config.data_range[2]
     # loop into the file locations to get the data
     data = []
-    if config.suffix == None:
+    if config.suffix is None:
         config.suffix = 'dat'
     for prefix in config.sim_list:
-        data.append(reg.Make_data_frames(prefix,suffix=config.suffix))
+        data.append(reg.Make_data_frames(prefix, suffix=config.suffix))
     # split data into groups
-    reg.Create_final_dataset_splits(data,config.n_sets,mix=config.mix)
-    reg.plot_distrabution(data,config.n_sets,config.systems_list,mix=config.mix)
+    reg.Create_final_dataset_splits(data, config.n_sets, mix=config.mix)
+    reg.plot_distrabution(
+        data,
+        config.n_sets,
+        config.systems_list,
+        mix=config.mix)
     # add in the AUC if needed
-    if config.path_auc != None:
-        reg.get_AUC(config.path_auc,config.systems_list)
+    if config.path_auc is not None:
+        reg.get_AUC(config.path_auc, config.systems_list)
     reg.pandafy(config.systems_list)
     if config.method == "LOOCV":
         reg.run_leave_one_out()
     elif config.method == "KFOLD":
-         #$untouched only matters in k fold
-        if config.untouched_num != None:
+        # $untouched only matters in k fold
+        if config.untouched_num is not None:
             reg.untouchedData(nset=config.untouched_num)
-        reg.kfold_runner(tolerance=config.data_tol,purge_cutoff=config.purge_cutoff,cv_metric=config.metric,flipp=config.flipp)
+        reg.kfold_runner(
+            tolerance=config.data_tol,
+            purge_cutoff=config.purge_cutoff,
+            cv_metric=config.metric,
+            flipp=config.flipp)
+
+
 main(config_file)
