40986256077e99c0de8f257e094e60bd8a5a3aa6
krb0073 <krb0073@mix.wvu.edu>
2021-10-13 11:53:01 -0400

    reformated some minor things

--- Solvent_box/make_random_box.py
+++ Solvent_box/make_random_box.py
@@ -4,8 +4,12 @@
 # @Filename: final_mox_maker.py
 # @Last modified by:   kbillings
 # @Last modified time: 2021-09-07T09:56:41-04:00
-""" this code takes a given pdb of a solvent moleculae an places the a items
-at random points in a given space this verson follows https://github.com/GrossfieldLab/loos/blob/main/Packages/OptimalMembraneGenerator/add_molecules.py"""
+
+"""
+this code takes a given pdb of a solvent moleculae an places the a items
+at random points in a given space this verson follows 
+https://github.com/GrossfieldLab/loos/blob/main/Packages/OptimalMembraneGenerator/add_molecules.py
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 import loos
@@ -18,18 +22,16 @@ number_of_mol = float(sys.argv[2])
 box_side_lenght = float(sys.argv[3])
 out_pdb = sys.argv[4]
 
-# vatiable that i define befor the loops
+# varables that i define befor the loops
 
 # max number of trys before we exit
 max = 2000
 trial = 0
 good = 0
-#cnt
 # load the pdb
 system = loos.PDB(single_pdb)
 box = loos.GCoord(box_side_lenght, box_side_lenght, box_side_lenght) # point of the farets point
 half_box = box_side_lenght *.5 # half the box
-
 solv = loos.AtomicGroup() # empty atomic group instance to append molecules
 
 # start ton add stuff in
