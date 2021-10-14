364ed2bc70b83d9bc39c96b68f5724932eea8e80
krb0073 <krb0073@mix.wvu.edu>
2021-10-13 23:48:36 -0400

    added the code to cobime the bulk lipids and protein with the new protein structure

---
+++ Ret_codes/COMBINE_GOOD.tcl
@@ -0,0 +1,25 @@
+# thiss attachedb the ret to the portein
+package require psfgen
+# make the segments
+foreach S  {  189 185 182} {
+	for {set i 1} {$i <= 3} {incr i} {
+		resetpsf 
+		psfcontext reset
+		set my_path "/media/bak22/BR_Photo/BR/newer_mutants/alltrans/$S/$i"
+		# now load in the final pdb using the thing 
+		mol load psf ${my_path}/final.psf pdb ${my_path}/final.pdb
+		set A [atomselect top "not (protein or resname RET RTNH)"]
+		$A writepsf ${my_path}/temp.psf
+		$A writepdb ${my_path}/temp.pdb 
+		mol delete all 
+		# now we can make the fixed_pdb and psf 
+		readpsf ${my_path}/temp.psf
+		coordpdb ${my_path}/temp.pdb 
+		readpsf ${my_path}/regen_ret.psf
+		coordpdb ${my_path}/regen_ret.pdb 
+		writepsf ${my_path}/final_2.psf
+		writepdb ${my_path}/final_2.pdb
+	}
+}
+
+exit
