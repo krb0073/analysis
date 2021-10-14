82a4460af4407238a91bca9ce7189a31633541fe
krb0073 <krb0073@mix.wvu.edu>
2021-10-13 23:50:48 -0400

    the code  i made to reattach the retainl from the move/algined system

---
+++ Ret_codes/V__2_attach_RET.tcl
@@ -0,0 +1,33 @@
+# thiss attachedb the ret to the portein
+package require psfgen
+topology /media/bak11/toppar/top_all36_prot.rtf
+topology /media/bak11/toppar/retinal-pro.str
+pdbalias residue HIS HSE
+pdbalias atom ILE CD1 CD
+pdbalias atom HOH OW OH2
+pdbalias residue HOH TIP3
+# make the segments
+foreach S  {  189 185 182} {
+	for {set i 1} {$i <= 3} {incr i} {
+		set my_path "/media/bak22/BR_Photo/BR/newer_mutants/alltrans/$S/$i"
+		puts $S
+		resetpsf
+	# retinal
+#	segment RET {pdb ret_only.pdb}
+#	coordpdb ret_only.pdb RET
+	# proteins
+		segment PRO {pdb ${my_path}/regen.pdb}
+		coordpdb  ${my_path}/regen.pdb PRO
+		patch ASPP PRO:96
+		regenerate angles dihedrals
+		patch GLUP PRO:194
+		regenerate angles dihedrals
+		patch SCK1 PRO:216 PRO:250
+		regenerate angles dihedrals
+		guesscoord
+		writepsf ${my_path}/regen_ret.psf
+		writepdb ${my_path}/regen_ret.pdb
+	}
+}
+
+exit
