a5596d16d314854b82455c54b1fb62406ddde5e1
krb0073 <krb0073@mix.wvu.edu>
2021-10-13 23:52:19 -0400

    uses pymol to algin the protein with the center of the protein from the bilayer. This is used after PRO_only.tcl is run

---
+++ Ret_codes/algin_fix_ret.py
@@ -0,0 +1,20 @@
+""" 
+this code will take the systems  with the regenatred system and place 
+the new retinal
+"""
+systes = "../newer_mutants/alltrans/"
+mut = ['182' , '185' , '189']
+for m in mut: # loop into all mutants
+	# there are three replacates of each 
+	for st in range(1,4):
+		cmd.load(f'ret_1IW6_{m}.pdb','good')
+		# load the  full system and the new boys 
+		cmd.load(f"{systes}/{m}/{str(st)}/pro_only.pdb",f"{m}-{str(st)}")
+		# select the protein atoms 
+		cmd.align("good",f"{m}-{str(st)}")
+		cmd.save(f"{systes}/{m}/{str(st)}/regen.pdb" , "good")
+		cmd.remove('all')
+		cmd.delete('all')
+
+
+
