a8118902d1ba8646d3cfc935cf1ff602a6b9cae6
krb0073 <krb0073@mix.wvu.edu>
2021-11-01 18:43:42 -0400

    updated

---
+++ slow_eq/exstend_for_time.py
@@ -0,0 +1,6 @@
+# @Author: Kyle Billings <kbillings>
+# @Date:   2021-10-14T14:48:58-04:00
+# @Email:  krb0073@mix.wvu.edu
+# @Filename: exstend_for_time.py
+# @Last modified by:   kbillings
+# @Last modified time: 2021-10-14T14:48:58-04:00
